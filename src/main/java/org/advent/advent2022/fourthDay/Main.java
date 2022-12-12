2bf5828905ec955e4b09d83c2e8e222d85f13c35
AdamGniadzik <pikmax@windowslive.com>
2022-12-12 19:56:26 +0100

    Fourth day

---
+++ src/main/java/org/advent/advent2022/fourthDay/Main.java
@@ -0,0 +1,62 @@
+package org.advent.advent2022.fourthDay;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;Fourt
+
+public class Main {
+    public static void main(String[] args) {
+        int score = 0;
+        //First part
+        try {
+            InputStream is = Main.class.getClassLoader().getResourceAsStream("org/advent/advent2022/fourthDay/input.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            // Part 1
+            while (br.ready()) {
+                String[] elf = br.readLine().split(",");
+                String firstElfNumbers[] = elf[0].split("-");
+                String secondElfNumbers[] = elf[1].split("-");
+                int firstElfBegin = Integer.parseInt(firstElfNumbers[0]);
+                int firstElfEnd = Integer.parseInt(firstElfNumbers[1]);
+                int secondElfBegin = Integer.parseInt(secondElfNumbers[0]);
+                int secondElfEnd = Integer.parseInt(secondElfNumbers[1]);
+                if(firstElfBegin <= secondElfBegin && firstElfEnd >= secondElfEnd){
+                    score ++;
+                }else if (secondElfBegin <= firstElfBegin && secondElfEnd >= firstElfEnd){
+
+                    score ++;
+                }
+            }
+            System.out.println(score);
+
+
+            score=0;
+            br.close();
+            is = org.advent.advent2022.thirdDay.Main.class.getClassLoader().getResourceAsStream("org/advent/advent2022/fourthDay/input.txt");
+            br = new BufferedReader(new InputStreamReader(is));
+
+            //Part 2
+            while (br.ready()) {
+                String[] elf = br.readLine().split(",");
+                String[] firstElfNumbers = elf[0].split("-");
+                String[] secondElfNumbers = elf[1].split("-");
+                int firstElfBegin = Integer.parseInt(firstElfNumbers[0]);
+                int firstElfEnd = Integer.parseInt(firstElfNumbers[1]);
+                int secondElfBegin = Integer.parseInt(secondElfNumbers[0]);
+                int secondElfEnd = Integer.parseInt(secondElfNumbers[1]);
+                if(firstElfEnd < secondElfBegin || firstElfBegin > secondElfEnd){
+
+                }else{
+                    score++;
+                }
+            }
+
+            System.out.println(score);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
