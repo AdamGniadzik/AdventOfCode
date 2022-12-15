d0d35d8dc32655f6a7eb7b317fc6dab172507e47
AdamGniadzik <pikmax@windowslive.com>
2022-12-15 01:18:27 +0100

    --- Day 6: Tuning Trouble ---

---
+++ src/main/java/org/advent/advent2022/sixthDay/Main.java
@@ -0,0 +1,44 @@
+package org.advent.advent2022.sixthDay;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            final int PART_ONE_CONDITION = 4;
+            final int PART_TWO_CONDITION = 4;
+            InputStream is = Main.class.getClassLoader().getResourceAsStream("org/advent/advent2022/sixthDay/input.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            Map<Character, Integer> markerMap = new HashMap<>();
+            String input = br.readLine();
+            int steps = 0;
+            int subStep = 0;
+            while (steps < input.length()) {
+                Integer prevValue = markerMap.put(input.charAt(steps), subStep);
+                if (prevValue != null) {
+                    steps = steps - (subStep - prevValue);
+                    subStep = 0;
+                    markerMap.clear();
+                }
+                //Replace variable to complete both parts of challenge.
+                if (subStep == PART_TWO_CONDITION) {
+                    System.out.println(markerMap);
+                    System.out.println(steps + 1);
+                    break;
+                }
+                steps++;
+                subStep++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
