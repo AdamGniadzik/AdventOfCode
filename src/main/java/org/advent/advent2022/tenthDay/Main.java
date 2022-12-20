c57479cbedde817e5ba8887083979b6d6428abb5
AdamGniadzik <pikmax@windowslive.com>
2022-12-20 10:49:22 +0100

    --- Day 10: Cathode-Ray Tube ---

---
+++ src/main/java/org/advent/advent2022/tenthDay/Main.java
@@ -0,0 +1,73 @@
+package org.advent.advent2022.tenthDay;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            InputStream is = Main.class.getClassLoader().getResourceAsStream("org/advent/advent2022/tenthDay/input.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            Set<Integer> set = Set.of(20, 60, 100, 140, 180, 220);
+            int counter = 1;
+            int state = 1;
+            int sum = 0;
+            //Part 1
+//            while (br.ready()) {
+//                String line = br.readLine();
+//                counter++;
+//                if (line.startsWith("addx")) {
+//                    if (set.contains(counter)) {
+//                        sum += state * counter;
+//                    }
+//                    state += Integer.parseInt(line.substring(5));
+//                    counter++;
+//                }
+//                if (set.contains(counter)) {
+//                    sum += state * counter;
+//                }
+//
+//            }
+//            System.out.println(sum);
+            counter = 0;
+            state = 1;
+            while (br.ready()) {
+                String line = br.readLine();
+                if (counter == 40) {
+                    System.out.print("\n");
+                    counter = 0;
+                }
+                printPixel(counter, state);
+                counter++;
+                if (line.startsWith("addx")) {
+                    if (counter == 40) {
+                        counter = 0;
+                        System.out.print("\n");
+                    }
+                    printPixel(counter, state);
+                    state += Integer.parseInt(line.substring(5));
+                    counter++;
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printPixel(int counter, int state){
+        if(counter + 1 >= state && counter + 1 <= state + 2){
+            System.out.print('#');
+        }else{
+            System.out.print('.');
+        }
+    }
+
+}
