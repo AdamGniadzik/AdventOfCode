889c7554e0f4fd5cb3592032825d23bcb4080c80
AdamGniadzik <pikmax@windowslive.com>
2024-01-08 10:03:23 +0100

    --- 2023 Day 15: Lens Library ---

--- src/main/java/org/advent/advent2021/Main.java
+++ src/main/java/org/advent/advent2021/Main.java
@@ -2,6 +2,9 @@ package org.advent.advent2021;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("DZIEN DOBRY TU BOMBINSKI ");
+        System.out.println("DZIEN DOBRY TU BOMBINSKI " +
+                "" +
+                "" +
+                "BLA BLA BLA ");
     }
 }
